e48a1dc1936ea3c0dcfe3f66204f8a90e39074e6
Sambaram, Mr.Mahesh <smahesh@corp.untd.com>
2023-09-22 16:31:14 +0530

    Added all files to github

---
+++ Assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void findMaxChar(string str)
+        {
+            int[] freq = new int[128];
+            int maxCount = int.MinValue;
+            char maxChar = '\0';
+            foreach (char ch in str)
+            {
+                freq[ch]++;
+            }
+            for (int index = 0; index < freq.Length; index++)
+            {
+                if (freq[index] > maxCount)
+                {
+                    maxChar = (char)index;
+                    maxCount = freq[index];
+                }
+            }
+            Console.WriteLine("Max Occuring Character :{0}, {1} times", maxChar, maxCount);
+            Console.Write("Positions : ");
+            for (int index = 0; index < str.Length; index++)
+            {
+                if (maxChar == str.ElementAt(index))
+                {
+                    Console.Write(index + 1 + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            findMaxChar("UnitedOnline");
+        }
+    }
+}
